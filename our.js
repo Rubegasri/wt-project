0b3ad0bad7e5a7f187effa6c1af23b974a8431eb
Rubegasri <148534378+Rubegasri@users.noreply.github.com>
2023-10-21 07:26:50 +0530

    Add files via upload

---
+++ our.js
@@ -0,0 +1,8 @@
+$('.slider-container').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: true,
+    prevArrow: '<button class="slick-prev">Previous</button>',
+    nextArrow: '<button class="slick-next">Next</button>',
+});
